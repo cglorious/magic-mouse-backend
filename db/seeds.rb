f65caa88e94d1233ab5c3ab6e6b1dc06b8b67c19
Catherine Gloria <catherinegloria@gmail.com>
2021-04-06 05:26:09 -0700

    resort seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Resort.delete_all
+
+disneyland = Resort.create(
+  name: "Disneyland Resort",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/2/1280/720/75/dam/disneyland/home/destination/sleeping-beauty-castle-tint-16x9.jpg",
+  location: "Anaheim, CA, USA"
+)
+wdw = Resort.create(
+  name: "Walt Disney Resort",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/02/bcc92834982402984029834.jpg",
+  location: "Orlando, FL, USA"
+)
+tokyo = Resort.create(
+  name: "Tokyo Disney Resort",
+  image_url: "https://www.slashfilm.com/wp/wp-content/images/tokyo-disneyland-e1592926525256.jpeg",
+  location: "Tokyo, Japan"
+)
