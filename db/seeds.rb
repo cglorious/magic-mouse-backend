a088ef2d23a7bf5fdcbe5f68bb4b3bd6e5a68621
Catherine Gloria <catherinegloria@gmail.com>
2021-04-07 11:42:36 -0700

    update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -33,12 +33,12 @@ tokyo = Resort.create(
 disneyland = Park.create(
   name: "Disneyland",
   image_url: "https://insidethemagic-119e2.kxcdn.com/wp-content/uploads/2020/03/Disneyland-Entrance.jpg",
-  resort_id: dl.id
+  resort: dl
 )
 ca_adventure = Park.create(
   name: "California Adventure",
   image_url: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F28%2F2021%2F02%2F09%2Fdisney-dining-DIS0221-2000.jpg",
-  resort_id: dl.id
+  resort: dl
 )
 
 #Lands
@@ -46,55 +46,79 @@ star_wars = Land.create(
   name: "Star Wars: Galaxy's Edge",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/events-tours/disneyland/plan-star-wars-galaxys-edge/courtyard-wide-galaxys-edge-16x9.jpg?1605284178492",
   description: "Explore this out-of-this-world land, where you can live your adventure in a galaxy far, far away!",
-  park_id: disneyland.id
+  park: disneyland
 )
 fantasyland = Land.create(
   name: "Fantasyland",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/new-orleans-square/dlr-castle-day-exterior-16x9.jpg?1605284178493",
   description: "Step into fairy-tale worlds filled with Characters, attractions, entertainment and more!",
-  park_id: disneyland.id
+  park: disneyland
 )
 tomorrowland = Land.create(
   name: "Tomorrowland",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/tomorrowland/space-mountain-night-16x9_1.jpg?1605284178493",
   description: "Blast off with space-age attractions, Character encounters, dining and other galactic goodies.",
-  park_id: disneyland.id
+  park: disneyland
 )
 adventureland = Land.create(
   name: "Adventureland",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/adventureland/jungle-cruise-elephant-16x9.jpg?1605284178493",
   description: "Get in touch with your wild side in a jungle-esque setting filled with tiki-themed delights and exotic odysseys. ",
-  park_id: disneyland.id
+  park: disneyland
 )
 critter_country = Land.create(
   name: "Critter Country",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/wdpro-assets/dlr/parks-and-tickets/attractions/disneyland/splash-mountain/splash-mountain-00.jpg?1605284178493",
   description: "Prepare for a wild time—explore a forest filled with classic attractions, dining, shopping and more!",
-  park_id: disneyland.id
+  park: disneyland
 )
 frontierland = Land.create(
   name: "Frontierland",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/frontierland/big-thunder-mountain-night-16x9.jpg?1605284178493",
   description: "Blaze fun new trails like a real pioneer—explore attractions, entertainment, shopping, dining and more!",
-  park_id: disneyland.id
+  park: disneyland
 )
 mainstreet = Land.create(
   name: "Mainstreet, U.S.A.",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/main-street-usa/family-main-st-no-grad-16x9.jpg?1605284178493",
   description: "Enjoy a leisurely stroll through small-town America—you’ll love the turn-of-the-century charm!",
-  park_id: disneyland.id
+  park: disneyland
 )
 toontown = Land.create(
   name: "Mickey's Toontown",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/mickeys-toontown/mickey-minnie-toontown-16x9.jpg?1605284178493",
   description: "Discover topsy-turvy worlds with a dash of classic Disney—explore attractions, characters and more!",
-  park_id: disneyland.id
+  park: disneyland
 )
 nola = Land.create(
   name: "New Orleans Square",
   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/critter-country/dlr-haunted-mansion-16x9.jpg?1605284178493",
   description: "Jazz up your day—celebrate the colorful culture and spirit of this Dixie-lovin’ land!",
-  park_id: disneyland.id
+  park: disneyland
 )
 
 #Attractions
+millennium = Attraction.create(
+  name: "Millennium Falcon: Smugglers Run",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1300/544/75/dam/disneyland/destinations/disneyland/star-wars-galaxys-edge/photography/smugglers-run-16x9.jpg?1605249800957",
+  closure: false,
+  land: star_wars
+)
+resistance = Attraction.create(
+  name: "Star Wars: Rise of the Resistance",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/650/366/75/dam/disneyland/home/destination/rise-of-the-resisstance-interior-16x9.jpg?1605249800957",
+  closure: false,
+  land: star_wars
+)
+peter_pan = Attraction.create(
+  name: "Peter Pan's Flight",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/433/433/75/dam/disneyland/destinations/disneyland/fantasyland/peter-pan-flight-v2-1x1.jpg?1604384613389",
+  closure: false,
+  land: fantasyland
+)
+tea_party = Attraction.create(
+  name: "Mad Tea Party",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/433/433/75/dam/disneyland/destinations/disneyland/fantasyland/mad-tea-party-v2-1x1.jpg?1604384613389",
+  closure: false,
+  land: fantasyland
+)
---
+++ db/seeds/04_attractions.rb
@@ -0,0 +1,24 @@
+# @millennium = Attraction.create(
+#   name: "Millennium Falcon: Smugglers Run",
+#   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/1300/544/75/dam/disneyland/destinations/disneyland/star-wars-galaxys-edge/photography/smugglers-run-16x9.jpg?1605249800957",
+#   closure: false,
+#   land: @star_wars
+# )
+# @resistance = Attraction.create(
+#   name: "Star Wars: Rise of the Resistance",
+#   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/650/366/75/dam/disneyland/home/destination/rise-of-the-resisstance-interior-16x9.jpg?1605249800957",
+#   closure: false,
+#   land: @star_wars
+# )
+# @peter_pan = Attraction.create(
+#   name: "Peter Pan's Flight",
+#   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/433/433/75/dam/disneyland/destinations/disneyland/fantasyland/peter-pan-flight-v2-1x1.jpg?1604384613389",
+#   closure: false,
+#   land: @fantasyland
+# )
+# @tea_party = Attraction.create(
+#   name: "Mad Tea Party",
+#   image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/433/433/75/dam/disneyland/destinations/disneyland/fantasyland/mad-tea-party-v2-1x1.jpg?1604384613389",
+#   closure: false,
+#   land: @fantasyland
+# )
