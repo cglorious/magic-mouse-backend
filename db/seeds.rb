752e95d4905dae75d8a14182d049540162534c3b
Catherine Gloria <catherinegloria@gmail.com>
2021-04-07 08:40:20 -0700

    land seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -38,6 +38,59 @@ ca_adventure = Park.create(
 )
 
 #Lands
-
+star_wars = Land.create(
+  name: "Star Wars: Galaxy's Edge",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/events-tours/disneyland/plan-star-wars-galaxys-edge/courtyard-wide-galaxys-edge-16x9.jpg?1605284178492",
+  description: "Explore this out-of-this-world land, where you can live your adventure in a galaxy far, far away!",
+  park_id: disneyland.id
+)
+fantasyland = Land.create(
+  name: "Fantasyland",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/new-orleans-square/dlr-castle-day-exterior-16x9.jpg?1605284178493",
+  description: "Step into fairy-tale worlds filled with Characters, attractions, entertainment and more!",
+  park_id: disneyland.id
+)
+tomorrowland = Land.create(
+  name: "Tomorrowland",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/tomorrowland/space-mountain-night-16x9_1.jpg?1605284178493",
+  description: "Blast off with space-age attractions, Character encounters, dining and other galactic goodies.",
+  park_id: disneyland.id
+)
+adventureland = Land.create(
+  name: "Adventureland",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/adventureland/jungle-cruise-elephant-16x9.jpg?1605284178493",
+  description: "Get in touch with your wild side in a jungle-esque setting filled with tiki-themed delights and exotic odysseys. ",
+  park_id: disneyland.id
+)
+critter_country = Land.create(
+  name: "Critter Country",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/wdpro-assets/dlr/parks-and-tickets/attractions/disneyland/splash-mountain/splash-mountain-00.jpg?1605284178493",
+  description: "Prepare for a wild time—explore a forest filled with classic attractions, dining, shopping and more!",
+  park_id: disneyland.id
+)
+frontierland = Land.create(
+  name: "Frontierland",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/frontierland/big-thunder-mountain-night-16x9.jpg?1605284178493",
+  description: "Blaze fun new trails like a real pioneer—explore attractions, entertainment, shopping, dining and more!",
+  park_id: disneyland.id
+)
+mainstreet = Land.create(
+  name: "Mainstreet, U.S.A.",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/main-street-usa/family-main-st-no-grad-16x9.jpg?1605284178493",
+  description: "Enjoy a leisurely stroll through small-town America—you’ll love the turn-of-the-century charm!",
+  park_id: disneyland.id
+)
+toontown = Land.create(
+  name: "Mickey's Toontown",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/mickeys-toontown/mickey-minnie-toontown-16x9.jpg?1605284178493",
+  description: "Discover topsy-turvy worlds with a dash of classic Disney—explore attractions, characters and more!",
+  park_id: disneyland.id
+)
+nola = Land.create(
+  name: "New Orleans Square",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/resize/mwImage/1/434/244/75/dam/disneyland/destinations/disneyland/critter-country/dlr-haunted-mansion-16x9.jpg?1605284178493",
+  description: "Jazz up your day—celebrate the colorful culture and spirit of this Dixie-lovin’ land!",
+  park_id: disneyland.id
+)
 
 #Attractions
