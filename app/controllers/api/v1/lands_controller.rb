80540e3d9fc93e9e8e1221cb357e7dd452b5dcf4
Catherine Gloria <catherinegloria@gmail.com>
2021-04-08 05:17:03 -0700

    land actions and serializer

--- app/controllers/api/v1/lands_controller.rb
+++ app/controllers/api/v1/lands_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::LandsController < ApplicationController
+
+  def index
+    lands = Land.all
+    render json: LandSerializer.new(lands)
+  end
+
+  def show
+    land = Land.find(params[:id])
+    render json: LandSerializer.new(land)
+  end
+
 end
---
+++ app/serializers/land_serializer.rb
@@ -0,0 +1,4 @@
+class LandSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :image_url, :description, :park_id
+end
