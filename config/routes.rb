c5ca7d2d48e6033d112eadcd2ab00308ba1f2784
Catherine Gloria <catherinegloria@gmail.com>
2021-04-07 18:15:16 -0700

    namespace routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :attractions
-  resources :lands
-  resources :parks
-  resources :resorts
+  namespace :api do
+    namespace :v1 do
+      resources :attractions, only: [:index, :create, :show]
+      resources :lands, only: [:index, :show]
+      resources :parks, only: [:index, :show]
+      resources :resorts, only: [:index, :show]
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
