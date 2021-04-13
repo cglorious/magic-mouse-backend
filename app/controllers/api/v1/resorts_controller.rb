d75deaddfa899b77b60bdd6c186769add1c4687d
Catherine Gloria <catherinegloria@gmail.com>
2021-04-13 16:13:48 -0700

    add slug option to resorts

--- app/controllers/api/v1/resorts_controller.rb
+++ app/controllers/api/v1/resorts_controller.rb
@@ -5,6 +5,10 @@ class Api::V1::ResortsController < ApplicationController
     render json: ResortSerializer.new(resorts)
   end
 
+#add resources
+#change to slug
+#add slug in resources
+
   def show
     resort = Resort.find(params[:id])
     render json: ResortSerializer.new(resort)
