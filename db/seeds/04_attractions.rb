6a32dbfda11270f52c9904dbe4492523bad8de3a
Catherine Gloria <catherinegloria@gmail.com>
2021-04-24 14:33:02 -0700

    attraction seed data

--- db/seeds/04_attractions.rb
+++ db/seeds/04_attractions.rb
@@ -36,3 +36,66 @@
   land: @fantasyland,
   count: 0
 )
+
+#tomorrowland
+@buzz = Attraction.create(
+  name: "Buzz Lightyear's Space Ranger Spin",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b7/Buzz_Lightyear%27s_Space_Ranger_Spin_Entrance_%284730826024%29.jpg/250px-Buzz_Lightyear%27s_Space_Ranger_Spin_Entrance_%284730826024%29.jpg",
+  closure: false,
+  land: @tomorrowland,
+  count: 0
+)
+
+#adventureland
+@indiana = Attraction.create(
+  name: "Indiana Jones Adventure",
+  image_url: "https://upload.wikimedia.org/wikipedia/en/8/89/Disneyland_Indiana_Jones_Attraction_Poster.jpg",
+  closure: false,
+  land: @adventureland,
+  count: 0
+)
+
+#critter_country
+@splash_mountain = Attraction.create(
+  name: "Splash Mountain",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Splash_Mountain_at_Disneyland.JPG/250px-Splash_Mountain_at_Disneyland.JPG",
+  closure: false,
+  land: @critter_country,
+  count: 0
+)
+
+#frontierland
+@big_thunder = Attraction.create(
+  name: "Big Thunder Mountain Railroad",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f5/Big_Thunder_Mountain_Entrance_Sign_at_Magic_Kingdom.jpg/250px-Big_Thunder_Mountain_Entrance_Sign_at_Magic_Kingdom.jpg",
+  closure: false,
+  land: @frontierland,
+  count: 0
+)
+
+#mainstreet
+@railroad = Attraction.create(
+  name: "Disneyland Railroad",
+  image_url: "https://upload.wikimedia.org/wikipedia/en/6/6b/Disneyland_Railroad_Poster.png",
+  closure: false,
+  land: @mainstreet,
+  count: 0
+)
+
+#mickeys_toontown
+@gadget = Attraction.create(
+  name: "Gadget's Go Coaster",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1e/Disneyland-GadgetsGoCoaster-sign.jpg/250px-Disneyland-GadgetsGoCoaster-sign.jpg",
+  closure: false,
+  land: @toontown,
+  count: 0
+)
+
+#nola
+@pirates = Attraction.create(
+  name: "Pirates of the Caribbean",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Pirates_of_the_Caribbean_Entrance.JPG/250px-Pirates_of_the_Caribbean_Entrance.JPG",
+  closure: false,
+  land: @nola,
+  count: 0
+)
