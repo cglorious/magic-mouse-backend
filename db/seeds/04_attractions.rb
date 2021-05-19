04a9c3aa06b2475187712172ca6e32605eeec2a6
Catherine Gloria <catherinegloria@gmail.com>
2021-05-19 08:36:53 -0700

    update seed data

--- db/seeds/04_attractions.rb
+++ db/seeds/04_attractions.rb
@@ -17,21 +17,21 @@
 #fantasyland
 @dumbo = Attraction.create(
   name: "Dumbo the Flying Elephant",
-  image_url: "https://lh5.googleusercontent.com/euk8cg2ILaLiF89yvb7WuqypBRRiuJTfXpuVN4sXsBYOli7MpxKhVawem5H04FxJvF2FFTvLfSKEFh5jdBxa=w2560-h1210-rw",
+  image_url: "https://lh3.googleusercontent.com/fife/ABSRlIosM9DCM37BpsrTdPBCnpN_7aP5Aai3hq-tnDv1CcEpaW4-_wiSQ7XW4NWhWdv41euvC1YXrlvrnTQkRLen0qdXhksbsOEKA-JijyyRoGBJyQ4vexVkKMFqBoLcXrFxr0tg63QakmsQARIKBf63XfCWsMUQw8VwRMjg-u3hWHVoM1mawQUHubDv6-uf1denFvfjuN2vRERKLdMiA_ouGJtiLHlMA1BEqaFJi0H4Flr-OC_YDYFn-uG4auYvmP_1blFOhMf-0iTBz6KlG7ubr7Jdj-06j1zL47q1fnHtJ2l0jH_GW2JwqC7CEbBytBcS0fJlyzZiNPPTSiyLPbPAkNxXH9UC06G3QOr4jpOoUs0MtAMket2YYocyBXURX1CRBM5FLdb_JtJFGpaZjz3YXxtjfYol0FxImIcU9ykKMuZ7SjjVSewTignNVHBo3YigcXLHuwAxR0_sQu1SlCgQMxKUgVp3cPU3NItL530Bo7npY2NSmUXbvxuJkm-uNb9ZlvJc8UhmzcBdi8b8lfr0SpvA6fXUMjzNDibyewSNP1sgaFbzXoMzm0qEDyhFCHRf8YCheuimGIdGIp72mzJvm5fJ-2lliwPNiXAIPLSSYtz2vyqHytDQLrwKShMfOA-lcfgy5z4puVH6yyOP2sJRcEArPvEK_L1a9WrJwexxwJY6iMpDKxP6hXxz0sNtTNfIcga9cuVSTQJzrTqxSDH5sIGPdHu_0-ms9A=w791-h613-ft",
   land: @fantasyland,
   count: 0,
   log: "We hadn't planned to ride this, but the line was short enough that we decided to go for it. There's always a nice view of Fantasyland."
 )
 @alice = Attraction.create(
   name: "Alice in Wonderland",
-  image_url: "https://lh4.googleusercontent.com/DillLhWjqbE0qHWiSGfe2fOHOq2dAMOHK4EgUcn7RevLpq6V6EhKRKv2Pn040Y6k1xnWBVtx4rlUJPlPuQPj=w2560-h1210-rw",
+  image_url: "https://lh3.googleusercontent.com/fife/ABSRlIr0Mcb8Io7a-fy6lH3we50miY4h-5B0FVHmUtuwnx9YXrcEkxwV4dmRJ4zaZzmiZw9Kp3gFcpqPK333pkGXlwRhh8u-bebEncD73PkTOvEeomHkMlj7aavszg74FU_xaoDA02C39KogfN3BE7rjYbtEadKFcXJuWLri9UdcUWmckYB1qE_u4e2SVPR7dLA5AnWiMxU5JAxJLA5wqWo40h-VDtMA-X41Ty2jgBGnVOLnsXqxiLLwdAQckkwTsiUol0y6ideHPuDZA5qHIGKcom3PKmg3HkL0atZAvFakhnT4RsNnPXfuMtQv7ZaJYl6RHD88ALphQXDkuYXA6cXumMubBKHHu1Q0HCumaZg8SaLiM8dF2wThrcj6o5xsWXdML-feX50Lu385ijkJ_V51CzBX6goSfLJ_NYgS4HLO7FyLgmKkG4lmU_0CI_O96EGpiBAI6KWT4tMWC5INaoezElJFV4Z62SnXD9wWN38zqBNvjgGx-ku-pi_PA0o-GwkchDZX1o_YtdA_AKIBjUAsqhnS7gBc0qPItIBxzduIb3Nnz4ih5fDisFVcdnXw6vipxxHlP5boPsX6faJMae2dtbq4SsrTauCUgUP9fCS10N9gBQmVMudj0fV08NQPbIm80kxm1teNkSXNlMz-ECcZ7poMdix6fwbSVpQPh-excjrj7pK4OXECSsZEySSeC1aeAcOaGUReMbnAF5U8-eu4sokApqhUWuUk_Q=w957-h597-ft",
   land: @fantasyland,
   count: 0,
   log: "This was Robbie's first ride of the trip. He had a blast. He especially liked the Queen of Hearts and the card soldiers."
 )
 # @small_world = Attraction.create(
 #   name: "It's A Small World",
-#   image_url: "https://lh3.googleusercontent.com/DN57QHXYaTz4wMcxY7-HMyvD4s6_VzFpIJxODy_okan5NSlaqmRL1vhlhgpSjhuC0QyB1SU-IXgfORDyD-20=w1840-h1210-rw",
+#   image_url: "https://lh3.googleusercontent.com/fife/ABSRlIpWLQfHvw4zSZTafQSHauo7tuy5_R5AHToAd2OygOE7kwJfuL5way6cEgl8qI_9pGbiJ6bhZOe8prshz3vU-gIIwh93DEbfkVSoBYSgG_k5bze_I4LcBgXNVz4vlxJ7KKdOFOuSkB1GnTbE-mOOLYt582mp87oZSL_6QEshxzaCshUsKpejO7bxpMMU2p7QGf6s5rAqswqIw-SQKR0a3xqKV_tQ28H3LiianCh7_Lb88YYPV13xVYYrV_n7GoqAAxxC9ybmZJP1ru_6NGcy7-oQIhzj6YcQebAIv8FkEDKDjsCg2xFvOusnsaGh01wyVtpPC23TV52zy-ycnkKCgSnTwNdMVaz5bPO0AqNJUMS7X9jRX5tXiJJnQFGZTDkMDkAa8kAzRTEoEghNICA5UhyincoCvVJr0nMQuADJ-0tBxUKvUOndkRmr2ckXAwWecpxVmQxGxpcN1mh5NZZTibRDSChBrocH-Pc-yXPgcF-qW7wi7raaTn18759D3-1QYk9euXFH2NlpEamrfbFQylZDlJ8EOEHIhV_WwhLtVtsbUFkeaYmGpwFHg1ydF6nt6xvahxfi1YFh_UJZdJUZi4gEtv-m7oDheUMBGIu0jofkB5ZtXZotH7c5insbAODFSs5Un3zmY0LMb4XwTWnosMky5rG7Lr_Soh48zgXWUgK_PAdIAPSR0QqJhRjE2XeSfxne0lPVhdDtnh_a5hZrkM8fQnhMAzCPkA=w1048-h613-ft",
 #   land: @fantasyland,
 #   count: 0,
 #   log: "Robbie was a bit tired on this one, but we were able to get our own boat. It was a nice, relaxing trip around the world!"
@@ -58,7 +58,7 @@
 #critter_country
 @splash_mountain = Attraction.create(
   name: "Splash Mountain",
-  image_url: "https://lh3.googleusercontent.com/fife/ABSRlIrorNFTFkzCZRa_xyq-EVeKfvsChLqcs2--2dHfyNBfNe5bwATd5SYmzzHyXVOQLgywWI5eS-9hZhDYnFi1Np6GZWfPDK57B120Lfd61UGc83mOFUr4KiqSybBV-X3OwXJKK8jU3NJOK69lrB-fLvaz56GOPYrS5uYtaxid7JtiwRb29HRmMu0YcU-0KsrDjEZhm6yFKgp6oLCqASwIV00Za7pRlLTJ6H9sky4FhWdboMlFn_1Dhv2TV5_cMzn9U_DUTpfL3Z-lHfwuJF7fC_Dc3c2fkxr4dP6mL6FN6X25GPDwHylbsnPy2Pb3Xszni9rbsRrQFhwsQrsn8GF4xSR0PVUPK106h-Agw1Z0XJorssUWUphkw71DzoLCmra2Ym2jY_StcX8k_bPnh7TaDy3Y6V0mWrbv3sfCpyTahW6a2GETaXoPIfJczNLOF6ZgOU_g3qYtyOG2x8eP6hgX4wNF8koabB7nAMqijoTyrxMKGDs2miog_jOJh1g8e6YJypWEwvKFfSlmO4hqfuNAM_iPGmkNxmfwlTdlGFtvFIUxKzJWlRiB10XcvjHtLZgHRsITpuvzout7Ph0wntzPVn5hEyHBF1KLgna9hwBeg8z4OJ3R1aC4tUMMnuXcI8hXGRu7dvbANGvTCLquQqAXD1IvtDo7FMw6ODqrate-CpIi-b3-Zh0yKvOnAQrHasPr1OqUZ_oeJj_KJ-X9m_yfQaL2hhV6t5wyAg=w2560-h1210-ft",
+  image_url: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Splash_Mountain_at_Disneyland.JPG",
   land: @critter_country,
   count: 0,
   log: "This was our favorite ride of the day; it was Carolyn's first time. Even better - there was a 5 minute wait!"
@@ -67,7 +67,7 @@
 #frontierland
 @big_thunder = Attraction.create(
   name: "Big Thunder Mountain Railroad",
-  image_url: "https://lh5.googleusercontent.com/4shF0wHnpz4JAG8dXkAO_qOayTAosdE9PWTNwTUjF6K0PZnXvnrKRCodvJDJkGLutm2Anf3YOtLc7o2FuJFB=w2560-h1210-rw",
+  image_url: "https://cdn1.parksmedia.wdprapps.disney.com/media/blog/wp-content/uploads/2020/04/luweyl2b4tj24t.jpg",
   land: @frontierland,
   count: 0,
   log: "Carolyn enjoyed looking at the scenery while in line. And of course, the ride was just as fun as ever!"
